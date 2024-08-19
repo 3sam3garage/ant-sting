7dc3379d9e62b4cd04480fff1c98e5c998240507
3sam3 <iamdap91@gmail.com>
2024-08-19 17:23:59 +0900

    feat: request logger

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { CoreModule } from '@libs/core';
 import { ReportModule } from './report';
+import { RequestLoggerMiddleware } from './middlewares';
 
 @Module({
   imports: [CoreModule, ReportModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
