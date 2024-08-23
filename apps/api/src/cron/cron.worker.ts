2e6417349ed0b2996fba187f95b6f86d1e90cfb7
3sam3 <iamdap91@gmail.com>
2024-08-23 11:38:16 +0900

    chore: cron

--- apps/api/src/cron/cron.worker.ts
+++ apps/api/src/cron/cron.worker.ts
@@ -4,22 +4,22 @@ import * as child_process from 'node:child_process';
 
 @Injectable()
 export class CronWorker {
-  @Cron('30 9 * * *')
-  reportCrawling() {
-    Logger.debug('report crawling start');
-    child_process.execSync('npm run start:batch:report:invest');
-    child_process.execSync('npm run start:batch:report:market-info');
-    child_process.execSync('npm run start:batch:report:industry');
-    child_process.execSync('npm run start:batch:report:economy');
-    child_process.execSync('npm run start:batch:report:debenture');
-    child_process.execSync('npm run start:batch:report:stock');
-    Logger.debug('report crawling done');
-  }
-
-  @Cron('30 10 * * *')
-  reportSummary() {
-    Logger.debug('report summary start');
-    child_process.execSync('start:batch:report:summary');
-    Logger.debug('report summary done');
-  }
+  // @Cron('30 9 * * *')
+  // reportCrawling() {
+  //   Logger.debug('report crawling start');
+  //   child_process.execSync('npm run start:batch:report:invest');
+  //   child_process.execSync('npm run start:batch:report:market-info');
+  //   child_process.execSync('npm run start:batch:report:industry');
+  //   child_process.execSync('npm run start:batch:report:economy');
+  //   child_process.execSync('npm run start:batch:report:debenture');
+  //   child_process.execSync('npm run start:batch:report:stock');
+  //   Logger.debug('report crawling done');
+  // }
+  //
+  // @Cron('30 10 * * *')
+  // reportSummary() {
+  //   Logger.debug('report summary start');
+  //   child_process.execSync('start:batch:report:summary');
+  //   Logger.debug('report summary done');
+  // }
 }
