a2e134ffd0b5a2cb549653b9461d9c19ab3ce536
3sam3 <iamdap91@gmail.com>
2024-11-27 02:43:02 +0900

    test: hana shinhan

--- apps/api/src/spec/service/hana-crawl.spec.ts
+++ apps/api/src/spec/service/hana-crawl.spec.ts
@@ -1,33 +1,96 @@
 import axios from 'axios';
+import pdf from 'pdf-parse';
 import { parse as parseHTML } from 'node-html-parser';
 import { joinUrl } from '@libs/common';
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from '@aws-sdk/client-bedrock-runtime';
+import { PDF_PARSING_PROMPT } from '../constants';
+
+const summary = {
+  title: '도쿄일렉트론 (8035 JP): 가이던스 상향에도 부진한 주가 흐름 지속',
+  href: 'https://www.hanaw.com/main/research/research/download.cmd?bbsSeq=1280907&attachFileSeq=1&bbsId=&dbType=&bbsCd=4003',
+};
+
+it('figuring market', () => {
+  const [match] =
+    '시세이도(4911.JP): 3Q24 Review: 컨센서스 하회, 가이던스 하향'?.match(
+      /\.\w{2}/,
+    );
+
+  const market = match?.replace('.', '')?.trim();
+  console.log(market);
+});
 
 describe('hana-crawler', () => {
+  const modelId = 'anthropic.claude-3-5-sonnet-20240620-v1:0';
+  let client: BedrockRuntimeClient;
   const HANA_BASE_URL = 'https://www.hanaw.com';
 
   beforeEach(() => {
     process.env.AWS_PROFILE = 'dev';
     process.env.AWS_PROFILEAWS_SDK_LOAD_CONFIG = '1';
+    client = new BedrockRuntimeClient({ region: 'us-east-1' });
   });
 
   it('list', async () => {
     const response = await axios.get(
-      'https://www.hanaw.com/main/research/research/RC_000000_M.cmd',
+      'https://www.hanaw.com/main/research/research/list.cmd?pid=8&cid=3',
       { responseType: 'json' },
     );
 
     const html = parseHTML(response.data);
 
-    const items = html.querySelectorAll(
-      '#container div.rc_main-list > div.box dl',
+    const rows = html.querySelectorAll(
+      '#container div.daily_bbs > ul > li > div.con',
     );
-    for (const item of items) {
-      const titleAnchor = item.querySelector('dt > a');
-      const title = titleAnchor?.innerText?.trim();
-      const href = titleAnchor?.getAttribute('href');
 
-      console.log(title);
-      console.log(joinUrl(HANA_BASE_URL, href));
+    const items = [];
+    for (const row of rows) {
+      const titleAnchor = row.querySelector('a.more_btn');
+      const title = titleAnchor?.innerText?.trim();
+      const fileAnchor = row.querySelector('a.j_fileLink');
+      const href = fileAnchor?.getAttribute('href');
+
+      items.push({ title, href: joinUrl(HANA_BASE_URL, href) });
     }
+
+    console.log(items);
+  });
+
+  it('pdf summary', async () => {
+    const item = await axios.get(summary.href, { responseType: 'arraybuffer' });
+    const data = await pdf(item.data, { max: 2 });
+
+    const query = PDF_PARSING_PROMPT.replace(
+      '{{PDF_EXTRACTED_TEXT}}',
+      data.text,
+    );
+
+    const payload = {
+      anthropic_version: 'bedrock-2023-05-31',
+      max_tokens: 1000,
+      messages: [
+        { role: 'user', content: [{ type: 'text', text: query }] },
+        { role: 'assistant', content: [{ type: 'text', text: '{' }] },
+      ],
+    };
+
+    const command = new InvokeModelCommand({
+      contentType: 'application/json',
+      body: JSON.stringify(payload),
+      modelId,
+    });
+
+    const apiResponse = await client.send(command);
+
+    const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
+    const responseBody = JSON.parse(decodedResponseBody);
+
+    const json = JSON.parse('{' + responseBody.content[0].text);
+
+    console.log(json);
+    return json;
   });
 });
--- apps/api/src/spec/service/shinhan-crawl.spec.ts
+++ apps/api/src/spec/service/shinhan-crawl.spec.ts
@@ -1,10 +1,23 @@
 import { eucKR2utf8 } from '@libs/common';
 import axios from 'axios';
+import pdf from 'pdf-parse';
+import { PDF_PARSING_PROMPT } from '../constants';
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from '@aws-sdk/client-bedrock-runtime';
 
 describe('shinhan-crawler', () => {
+  const modelId = 'anthropic.claude-3-5-sonnet-20240620-v1:0';
+  let client: BedrockRuntimeClient;
+
+  const file =
+    'https://bbs2.shinhansec.com/board/message/file.do?attachmentId=330309';
+
   beforeEach(() => {
     process.env.AWS_PROFILE = 'dev';
     process.env.AWS_PROFILEAWS_SDK_LOAD_CONFIG = '1';
+    client = new BedrockRuntimeClient({ region: 'us-east-1' });
   });
 
   it('list', async () => {
@@ -40,7 +53,7 @@ describe('shinhan-crawler', () => {
     console.log(items);
   });
 
-  it('detail', async () => {
+  it.skip('detail page', async () => {
     const response = await axios.get(
       'https://bbs2.shinhansec.com/mobile/view.do?boardName=foreignstock&messageId=906169&messageNumber=2790',
       { responseType: 'arraybuffer' },
@@ -48,5 +61,40 @@ describe('shinhan-crawler', () => {
 
     const text = eucKR2utf8(response.data);
     console.log(text);
+  });
+
+  it('pdf summary', async () => {
+    const item = await axios.get(file, { responseType: 'arraybuffer' });
+    const data = await pdf(item.data, { max: 2 });
+
+    const query = PDF_PARSING_PROMPT.replace(
+      '{{PDF_EXTRACTED_TEXT}}',
+      data.text,
+    );
+
+    const payload = {
+      anthropic_version: 'bedrock-2023-05-31',
+      max_tokens: 1000,
+      messages: [
+        { role: 'user', content: [{ type: 'text', text: query }] },
+        { role: 'assistant', content: [{ type: 'text', text: '{' }] },
+      ],
+    };
+
+    const command = new InvokeModelCommand({
+      contentType: 'application/json',
+      body: JSON.stringify(payload),
+      modelId,
+    });
+
+    const apiResponse = await client.send(command);
+
+    const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
+    const responseBody = JSON.parse(decodedResponseBody);
+
+    const json = JSON.parse('{' + responseBody.content[0].text);
+
+    console.log(json);
+    return json;
   });
 });
