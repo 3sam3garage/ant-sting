dd64c40e367a4d4eae107e72c76491af891fd6c8
3sam3 <iamdap91@gmail.com>
2024-12-05 14:09:47 +0900

    chore: scrape-all script

---
+++ scrape-all.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+nest start batch -- stock scrape-naver
+nest start batch -- stock scrape-hana
+nest start batch -- stock scrape-shinhan
+nest start batch -- stock scrape-kiwoom
+nest start batch -- economic-information scrape-naver
+nest start batch -- economic-information scrape-kcif
