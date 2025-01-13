33803742b366c72c4691dc599e4b7729dccade81
3sam3 <iamdap91@gmail.com>
2025-01-13 14:18:44 +0900

    feat: ticker api

--- apps/api/src/filing/services/filing.service.ts
+++ apps/api/src/filing/services/filing.service.ts
@@ -1,16 +1,10 @@
-import { Redis } from 'ioredis';
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { FilingRepository } from '@libs/domain';
-import { REDIS_NAME } from '@libs/config';
 import { FindFilingsQuery } from '../dto';
 
 @Injectable()
 export class FilingService {
-  constructor(
-    @Inject(REDIS_NAME.ANT_STING)
-    private redis: Redis,
-    private readonly repo: FilingRepository,
-  ) {}
+  constructor(private readonly repo: FilingRepository) {}
 
   async findByTickers(query: FindFilingsQuery) {
     return await this.repo.find({
