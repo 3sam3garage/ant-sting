859c20170f74a5df914cbf77d7b7aa4d1e2c5c03
3sam3 <iamdap91@gmail.com>
2025-01-15 12:35:21 +0900

    chore: scrape script

--- scrape-all.sh
+++ scrape-all.sh
@@ -4,9 +4,12 @@ nest start batch -- stock-report scrape-naver
 nest start batch -- stock-report scrape-hana
 nest start batch -- stock-report scrape-shinhan
 nest start batch -- stock-report scrape-kiwoom
+
 nest start batch -- economic-information scrape-naver
 nest start batch -- economic-information scrape-kcif
 nest start batch -- economic-information exchange-rate
 nest start batch -- economic-information bond-yield
 nest start batch -- economic-information interest-rate
 nest start batch -- economic-information stock-index
+
+nest start batch -- stock scrape-sec-ticker
