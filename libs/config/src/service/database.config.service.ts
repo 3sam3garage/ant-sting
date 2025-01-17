55aa185c60fea28a40677487b13bfb12e596e8bc
3sam3 <iamdap91@gmail.com>
2025-01-17 12:57:18 +0900

    chore: typeorm redis cache

--- libs/config/src/service/database.config.service.ts
+++ libs/config/src/service/database.config.service.ts
@@ -1,17 +1,28 @@
 import { Inject, Injectable } from '@nestjs/common';
-import databaseConfig, { DatabaseConfig } from '../source/database.config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigType } from '@nestjs/config';
+import databaseConfig from '../source/database.config';
+import redisConfig from '../source/redis.config';
 
 @Injectable()
 export class DatabaseConfigService {
   constructor(
-    @Inject(databaseConfig.KEY) private readonly config: DatabaseConfig,
+    @Inject(databaseConfig.KEY)
+    private readonly config: ConfigType<typeof databaseConfig>,
+    @Inject(redisConfig.KEY)
+    private readonly redisConf: ConfigType<typeof redisConfig>,
   ) {}
 
   getConfig(): TypeOrmModuleOptions {
     return {
       ...this.config,
       entities: ['./dist/**/*.entity.js', './libs/**/*.entity.js'],
+      cache: {
+        type: 'ioredis',
+        options: {
+          ...this.redisConf,
+        },
+      },
     };
   }
 }
